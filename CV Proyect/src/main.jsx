0666493647a6251ed7eee0448da907da0951bb27
QuimCode <quimafoncode@gmail.com>
2024-12-04 23:48:17 -0300

    Chequeo en el main de cambios html

--- CV Proyect/src/main.jsx
+++ CV Proyect/src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Message from './Message.jsx'
+import TodoList from './TooList.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <Message message="Hello, World!" />
+    <TodoList />
   </StrictMode>,
 )
