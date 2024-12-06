39953a90b8b441c4eb6f3c7eb3368cd24fd0d4b4
QuimCode <quimafoncode@gmail.com>
2024-12-05 22:44:19 -0300

    Cambios en main

--- CV Proyect/src/main.jsx
+++ CV Proyect/src/main.jsx
@@ -4,12 +4,10 @@ import './index.css'
 import App from './App.jsx'
 import Message from './Message.jsx'
 import TodoList from './TooList.jsx'
-import Navbar from './Navbar.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-    <Navbar></Navbar>
     <Message message="Hello, World!" />
     <TodoList />
   </StrictMode>,
